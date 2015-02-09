bcaa0d5ed2759fb520bd926931a393ebe382c5c8
Chris Brackert <cbrackert@gmail.com>
2015-02-09 11:48:07 -0800

    Fixes #13. Fixes Host property title typo

--- socketio_block.py
+++ socketio_block.py
@@ -175,7 +175,7 @@ class SocketIO(Block):
             FROM the SocketIo room.
 
     """
-    host = StringProperty(title='SocketIo Hose', default="127.0.0.1")
+    host = StringProperty(title='SocketIo Host', default="127.0.0.1")
     port = IntProperty(title='Port', default=443)
     room = StringProperty(title='SocketIo Room', default="default")
     content = ExpressionProperty(
