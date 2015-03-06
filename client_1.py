fc9b144115d8e84ad9671b395621fc1d6dd1dd37
mattdodge <matt@n.io>
2015-03-06 11:47:43 -0800

    Simplifies the v1 client code by removing methods that it hasn't changed from its parent class

--- client_1.py
+++ client_1.py
@@ -1,39 +1,15 @@
 import json
 from .client import SocketIOWebSocketClient
-from nio.modules.threading import Thread
 
 
 class SocketIOWebSocketClientV1(SocketIOWebSocketClient):
 
     """ A client for 1.0 socket.io servers """
 
-    def __init__(self, url, logger, room, listen,
-                 restart_handler, data_handler):
-        super(SocketIOWebSocketClient, self).__init__(url, None, None)
-        self._th = Thread(target=self.run, name='SocketIOWebSocketClient')
-        self._logger = logger
-        self._room = room
-        self._listen = listen
-        self._restart_handler = restart_handler
-        self._data_handler = data_handler
-
-    def handshake_ok(self):
-        self._th.start()
-        self._th.join(timeout=1.0)
-
     def opened(self):
         self._logger.info("Socket connection open")
         # Send a connection request
         self._send_packet(52)
-
-    def closed(self, code, reason=None):
-        self._logger.info(
-            "Socket connection closed {0}:{1}".format(code, reason))
-        self.handle_disconnect()
-
-    def handle_disconnect(self):
-        self._logger.info("Disconnection detected")
-        self._restart_handler()
 
     def received_message(self, m):
         # The message type comes as the first two digits
@@ -63,12 +39,6 @@ class SocketIOWebSocketClientV1(SocketIOWebSocketClient):
 
         message_handlers[message_type](message_data)
 
-    def send_msg(self, msg):
-        self._send_packet(3, '', '', msg)
-
-    def send_dict(self, data):
-        self._send_packet(4, '', '', json.dumps(data))
-
     def send_event(self, event, data):
         self._send_packet(42, event, data)
 
@@ -87,36 +57,8 @@ class SocketIOWebSocketClientV1(SocketIOWebSocketClient):
                                                         str(e))
             )
 
-    def _send_heartbeat(self):
-        self._logger.debug("Sending heartbeat message")
-        self._send_packet(2)
-
-    def _parse_message(self, message):
-        """parses a message, if it can"""
-        try:
-            obj = json.loads(message)
-        except Exception:
-            obj = {'message': message}
-
-        return obj
-
-    def _recv_disconnect(self, data=None):
-        self.handle_disconnect()
-
-    def _recv_connect(self, data=None):
-        self._logger.info("Socket.io connection confirmed")
-
-        self._logger.debug("Joining room %s" % self._room)
-        self.send_event('ready', self._room)
-
     def _recv_heartbeat(self, data=None):
         self._logger.debug("Heartbeat PONG received")
-
-    def _recv_msg(self, data=None):
-        self._data_handler(self._parse_message(data))
-
-    def _recv_json(self, data=None):
-        self._data_handler(self._parse_message(data))
 
     def _recv_event(self, data=None):
         # when we receive an event, we get a dictionary containing the event
@@ -131,12 +73,3 @@ class SocketIOWebSocketClientV1(SocketIOWebSocketClient):
             'event': event_data[0],
             'data': self._parse_message(event_data[1])
         })
-
-    def _recv_ack(self, data=None):
-        pass
-
-    def _recv_error(self, data=None):
-        pass
-
-    def _recv_noop(self, data=None):
-        pass
